cc57401278290fe43e561bd6a57f7a0c5586a666
Alex Fioto <afioto@icloud.com>
2021-01-14 13:19:26 -0500

    having fun with js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let colorButton = document.querySelector('button')
+let colorButton = document.getElementById('color-changer')
 let body = document.querySelector('body')
 let changeColor = function() {
     let x = Math.floor(Math.random() * 256);
@@ -9,3 +9,32 @@ let changeColor = function() {
 }
 
 colorButton.onclick = changeColor
+
+let musicButton = document.getElementById('musician-button')
+let dataScienceButton = document.getElementById('data-scientist-button')
+
+function musicTheme() {
+    let elements = document.getElementsByClassName('text')
+    let h2 = document.querySelector('h2')
+    for (i=0; i<elements.length; i++) {
+        elements[i].style.color = 'gold'
+        elements[i].style.fontFamily = "Chalkduster, fantasy"
+    }
+    body.style.backgroundColor = 'blue'
+    h2.innerHTML = 'Musician'
+}
+
+function dataScienceTheme() {
+    let elements = document.getElementsByClassName('text')
+    let h2 = document.querySelector('h2')
+    for (i=0; i<elements.length; i++) {
+        elements[i].style.color = 'rgb(2, 249, 14'
+        elements[i].style.fontFamily = "'Courier New', monospace"
+    }
+    body.style.backgroundColor = 'black'
+    h2.innerHTML = 'Data Scientist'
+}
+
+musicButton.onclick = musicTheme
+
+dataScienceButton.onclick = dataScienceTheme
